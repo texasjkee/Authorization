981581f70e7d8758b19f35b48231dc95fb1a7cdd
Michael <gt.gluck@gmail.com>
2024-04-16 17:45:04 +0300

    add: verifyRefreshTokenMiddleware

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,16 +1,43 @@
 const jwt = require('jsonwebtoken')
+const { signatureAccess } = require('./utils')
 
-module.exports = verifyAuthMiddleware = (req, res, next) => {
-  const token = req.headers.authorization ? req.headers.authorization.split('')[1] : ''
+const verifyAuthMiddleware = (req, res, next) => {
+  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : ''
 
-  if (!token) return res.sendStatus(401)
+  if (!token) {
+    return res.sendStatus(401)
+  }
 
   try {
-    const decoded = jwt.very(token, signatureAccess)
+    const decoded = jwt.verify(token, signatureAccess)
     req.user = decoded
   } catch (err) {
+    console.log(err)
     return res.sendStatus(401)
   }
 
   return next()
+}
+
+const verifyRefreshTokenMiddleware = (req, res, next) => {
+  const refreshToken = req.cookies.refreshToken
+
+  if (!refreshToken) {
+    return res.sendStatus(401)
+  }
+
+  try {
+    const decoded = jwt.verify(token, signatureAccess)
+    req.user = decoded
+  } catch (err) {
+    console.log(err)
+    return res.sendStatus(401)
+  }
+
+  return next()
+}
+
+module.exports = {
+  verifyAuthMiddleware,
+  verifyRefreshTokenMiddleware
 }
